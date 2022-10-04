eedb7f1ad27c40d04ed75a811e1b321c269e2a87
Ruben Plaza <rubenplaza2000@gmail.com>
2022-10-04 01:14:51 -0400

    implement page routing with react-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,44 @@
-import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 import "./App.css";
+import Patient from "./pages/Patient";
+import Doctor from "./pages/Doctor";
+import Home from "./pages/Home";
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <p>PATIENT MANAGEMENT SYSTEM</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/patient">Patient Page</Link>
+          </li>
+          <li>
+            <Link to="/doctor">Doctor Page</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+            A <Switch> looks through all its children <Route>
+            elements and renders the first one whose path
+            matches the current URL. Use a <Switch> any time
+            you have multiple routes, but you want only one
+            of them to render at a time
+        */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/patient" element={<Patient />} />
+          <Route path="/doctor" element={<Doctor />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
