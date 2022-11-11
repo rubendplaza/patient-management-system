6826c4667b633ea0de9542df71ee84feb1f6ec94
Nyle-FBM <nfmelegrito@gmail.com>
2022-11-10 19:14:44 -0500

    small commit

--- server/src/models/doctors/doctors.model.js
+++ server/src/models/doctors/doctors.model.js
@@ -1,18 +1,14 @@
-const doctors = require("./doctors.mongo");
+const Doctors = require("./doctors.mongo");
 
 // IMPLEMENT REQUIRED METHODS HERE.
 async function addDoctor(doctor) {
-  return await doctors.create(doctor);
+  return await Doctors.create(doctor);
 }
 
 async function getAllDoctors() {
-  return await doctors.find(
-    {},
-    {
-      _id: 0,
-      __v: 0,
-    }
-  );
+  return await Doctors.find()
+  .then(doctors => res.json(doctors))
+  .catch(err => res.status(400).json('Error: ' + err));
 }
 
 module.exports = {
