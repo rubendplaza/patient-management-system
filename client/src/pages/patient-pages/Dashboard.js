21e3abb5f12beb771db55adaa04baf1678fac363
Ruben Plaza <rubenplaza2000@gmail.com>
2022-12-01 20:08:29 -0500

    adding pdf functionality

--- client/src/pages/patient-pages/Dashboard.js
+++ client/src/pages/patient-pages/Dashboard.js
@@ -2,119 +2,127 @@
 
 // Coordinate with Nyle because this view might be the same as Patient.js in doctor-pages
 
-import userCircle from '../../images/User_cicrle_light.svg';
-import 'semantic-ui-css/semantic.min.css'
-import { Menu, Image, Container, Segment, List, Grid, Header, Table } from 'semantic-ui-react'
-import { NavLink, withRouter } from 'react-router-dom'
-import { useState } from 'react'
-import React, { useEffect } from 'react';
+import userCircle from "../../images/User_cicrle_light.svg";
+import "semantic-ui-css/semantic.min.css";
+import {
+  Menu,
+  Image,
+  Container,
+  Segment,
+  List,
+  Grid,
+  Header,
+  Table,
+} from "semantic-ui-react";
+import { NavLink, withRouter } from "react-router-dom";
+import { useState } from "react";
+import React, { useEffect } from "react";
 
 const Dashboard = (props) => {
-  const [name, setName] = useState('')
-	const [age, setAge] = useState('')
-  const [illness, setIllness] = useState('')
-	const [sex, setSex] = useState('')
-  const [doctor, setDoctor] = useState('')
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [illness, setIllness] = useState("");
+  const [sex, setSex] = useState("");
+  const [doctor, setDoctor] = useState("");
 
   const id = localStorage.getItem("id");
-  console.log(id); 
+  console.log(id);
 
   useEffect(() => {
-    getInfo()
-}, []); 
- 
-useEffect(() => {
-  getDoctor()
-}, []); 
+    getInfo();
+  }, []);
 
+  useEffect(() => {
+    getDoctor();
+  }, []);
 
   async function getInfo() {
+    const response = await fetch(
+      `http://localhost:8000/v1/patients/getbyid/${id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-		const response = await fetch(`http://localhost:8000/v1/patients/getbyid/${id}`, {
-     
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		})
-
-		const data = await response.json()
-		console.log(data); 
-    setName(data.name); 
-    setAge(data.age); 
-    setSex(data.sex); 
-    setIllness(data.knownIllnesses)
-    
-	}
-
+    const data = await response.json();
+    console.log(data);
+    setName(data.name);
+    setAge(data.age);
+    setSex(data.sex);
+    setIllness(data.knownIllnesses);
+  }
 
   async function getDoctor() {
+    const response = await fetch(
+      `http://localhost:8000/v1/patients/getdoctor/${id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-		const response = await fetch(`http://localhost:8000/v1/patients/getdoctor/${id}`, {
-     
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		})
-
-		const data = await response.json()
-		console.log(data); 
-    setDoctor(data.name)
-	}
+    const data = await response.json();
+    console.log(data);
+    setDoctor(data.name);
+  }
 
   return (
     <div>
-      <Menu color="blue" inverted fixed="top" size="large" borderless style={{borderBottomWidth: "5px", borderBottomColor: "black" }}>
-          <Menu.Item
-            as={NavLink} to="/dashboard"
-            name="Dashboard"
-            active={true}
-          >
-            Dashboard
-          </Menu.Item>
-          <Menu.Item
-            name="Schedule"
-            active={false}
-          >
-            Schedule
-          </Menu.Item>
-          <Menu.Item
-            as={NavLink} to="/reports"
-            name="Reports"
-            active={false}
-          >
-            Reports
-          </Menu.Item>
-          <Menu.Item
-            as={NavLink} to="/"
-            name="Icon"
-            active={false}
-            position="right"
-          >
-            <Image src={userCircle} size="mini"/>
-          </Menu.Item>
-        </Menu>
+      <Menu
+        color="blue"
+        inverted
+        fixed="top"
+        size="large"
+        borderless
+        style={{ borderBottomWidth: "5px", borderBottomColor: "black" }}
+      >
+        <Menu.Item as={NavLink} to="/dashboard" name="Dashboard" active={true}>
+          Dashboard
+        </Menu.Item>
+        <Menu.Item as={NavLink} to="/reports" name="Reports" active={false}>
+          Reports
+        </Menu.Item>
+        <Menu.Item
+          as={NavLink}
+          to="/"
+          name="Icon"
+          active={false}
+          position="right"
+        >
+          <Image src={userCircle} size="mini" />
+        </Menu.Item>
+      </Menu>
 
-        <Container fluid style={{ marginTop: "7em"}}>
-          <Grid centered padded relaxed columns={2} divided>
-            <Grid.Column verticalAlign="middle" width={2}>
-              <Image src={"/Images/JaneDoe.png"} circular />
-            </Grid.Column>
+      <Container fluid style={{ marginTop: "7em" }}>
+        <Grid centered padded relaxed columns={2} divided>
+          <Grid.Column verticalAlign="middle" width={2}>
+            <Image src={"/Images/JaneDoe.png"} circular />
+          </Grid.Column>
 
-            <Grid.Column>
-              <Segment padded raised style={{borderWidth: "2px", borderColor: "black", borderRadius: "25px"}}>
-                  <Segment vertical>Name: {name}</Segment>
-                  <Segment vertical>Sex: {sex}</Segment>
-                  <Segment vertical>Age: {age}</Segment>
-                  <Segment vertical>Known Illnesses: {illness}</Segment>
-                  <Segment vertical>Doctor: {doctor}</Segment>
-              </Segment>
-            </Grid.Column>
-          </Grid>
-
-      
-        </Container>
+          <Grid.Column>
+            <Segment
+              padded
+              raised
+              style={{
+                borderWidth: "2px",
+                borderColor: "black",
+                borderRadius: "25px",
+              }}
+            >
+              <Segment vertical>Name: {name}</Segment>
+              <Segment vertical>Sex: {sex}</Segment>
+              <Segment vertical>Age: {age}</Segment>
+              <Segment vertical>Known Illnesses: {illness}</Segment>
+              <Segment vertical>Doctor: {doctor}</Segment>
+            </Segment>
+          </Grid.Column>
+        </Grid>
+      </Container>
     </div>
   );
 };
--- client/src/pages/patient-pages/Reports.js
+++ client/src/pages/patient-pages/Reports.js
@@ -1,5 +1,6 @@
 // This is where Adam will code up the UI during first sprint.
-
+import { jsPDF } from "jspdf";
+import React, { useEffect, useState } from "react";
 import userCircle from "../../images/User_cicrle_light.svg";
 import { NavLink, withRouter } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
@@ -15,6 +16,93 @@ import {
 } from "semantic-ui-react";
 
 const Reports = (props) => {
+  const [todayDate, setTodayDate] = useState("");
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [bloodType, setBloodType] = useState("");
+  const [illness, setIllness] = useState("");
+  const [sex, setSex] = useState("");
+  const [doctor, setDoctor] = useState("");
+
+  useEffect(() => {
+    const tmpDate = new Date();
+    setTodayDate(String(tmpDate));
+    getInfo();
+  }, []);
+
+  useEffect(() => {
+    getDoctor();
+  }, []);
+
+  async function getInfo() {
+    const id = localStorage.getItem("id");
+    const response = await fetch(
+      `http://localhost:8000/v1/patients/getbyid/${id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const data = await response.json();
+    console.log(data);
+    setName(data.name);
+    setAge(data.age);
+    setSex(data.sex);
+    setBloodType(data.bloodType);
+    setIllness(data.knownIllnesses);
+  }
+
+  async function getDoctor() {
+    const id = localStorage.getItem("id");
+    const response = await fetch(
+      `http://localhost:8000/v1/patients/getdoctor/${id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const data = await response.json();
+    console.log(data);
+    setDoctor(data.name);
+  }
+
+  async function handleDownload() {
+    const patientId = localStorage.getItem("id");
+    const response = await fetch(
+      `http://localhost:8000/v1/patients/getbyid/${patientId}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const data = await response.json();
+    console.log("Received:", data);
+
+    const pdfText = [];
+    pdfText.push("COLLECTED DATE: " + todayDate);
+    pdfText.push("Name: " + name);
+    pdfText.push("Doctor Name: " + doctor);
+    pdfText.push("Age: " + age);
+    pdfText.push("Sex: " + sex);
+    pdfText.push("Blood Type: " + bloodType);
+    pdfText.push("Known Illessness: " + illness.join(","));
+
+    const doc = new jsPDF({
+      orientation: "landscape",
+    });
+
+    doc.text(pdfText, 10, 10);
+    doc.save("report.pdf");
+  }
   // Temporary test data
   const INFO = {
     img: "/Images/JaneDoe.png",
@@ -81,22 +169,18 @@ const Reports = (props) => {
         <Table celled>
           <Table.Header>
             <Table.Row>
-              <Table.HeaderCell>Report</Table.HeaderCell>
+              <Table.HeaderCell>Name</Table.HeaderCell>
               <Table.HeaderCell>Date</Table.HeaderCell>
-              <Table.HeaderCell>Notes</Table.HeaderCell>
               <Table.HeaderCell>PDF</Table.HeaderCell>
             </Table.Row>
           </Table.Header>
-          {INFO.reports.map((reportDetails) => (
-            <Table.Row>
-              <Table.Cell>{reportDetails.type}</Table.Cell>
-              <Table.Cell>{reportDetails.date}</Table.Cell>
-              <Table.Cell>{reportDetails.notes}</Table.Cell>
-              <Table.Cell>
-                <Image src="/Images/Out.svg" />
-              </Table.Cell>
-            </Table.Row>
-          ))}
+          <Table.Row>
+            <Table.Cell>{name}</Table.Cell>
+            <Table.Cell>{todayDate}</Table.Cell>
+            <Table.Cell onClick={handleDownload}>
+              <Image src="/Images/Out.svg" />
+            </Table.Cell>
+          </Table.Row>
         </Table>
       </Container>
     </div>
